7e62cfaa49ab9b023a8ba7e15db4435f2bdd6f1f
Robin Ludan <robinludan@gmx.com>
2023-11-27 10:40:49 +0200

    fixed formatting

--- .config/nvim/lua/robin/plugins/nvim-tree.lua
+++ .config/nvim/lua/robin/plugins/nvim-tree.lua
@@ -1,25 +1,24 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({})
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    -- set the keymaps
-    local keymap = vim.keymap
+		-- set the keymaps
+		local keymap = vim.keymap
 
-    -- neo-tree
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>")
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
-
-  end,
+		-- neo-tree
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
+		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>")
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
+	end,
 }
--- .config/nvim/lua/robin/plugins/scope.lua
+++ .config/nvim/lua/robin/plugins/scope.lua
@@ -16,7 +16,7 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>c", ":tabclose<cr>", { desc = "close the current working buffer" })
+		keymap.set("n", "<leader>cc", ":tabclose<cr>", { desc = "close the current working buffer" })
 		keymap.set("n", "<leader>nw", ":tabnew<cr>", { desc = "create a new tab" })
 		keymap.set("n", "<leader>tn", ":tabnext<cr>", { desc = "move to the next tab" })
 		keymap.set("n", "<leader>tp", ":tabprevious<cr>", { desc = "move to the previous tab" })
