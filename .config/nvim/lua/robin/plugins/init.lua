7e65e9aba7eec54deb4aec8422c7b4596a95ac6f
Robin Ludan <robinludan@gmx.com>
2023-12-12 09:39:39 +0200

    toggleterm

--- .config/nvim/lua/robin/plugins/init.lua
+++ .config/nvim/lua/robin/plugins/init.lua
@@ -1,4 +1,18 @@
 return {
 	-- quickly install packages that don't need a lot of customizations
 	"christoomey/vim-tmux-navigator", -- tmux and split terminal
+
+	-- toggleterm
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup()
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+			keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
+			keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>")
+			keymap.set("n", "<leader>tt", ":ToggleTerm direction=tab<CR>")
+		end,
+	},
 }
