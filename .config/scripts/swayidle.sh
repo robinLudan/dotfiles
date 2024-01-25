c805919914c8bcff540bd7555dcbba87d37ee455
Robin <robinludan@gxm.com>
2024-01-25 09:57:48 +0300

    autosleep

--- .config/scripts/swayidle.sh
+++ .config/scripts/swayidle.sh
@@ -2,7 +2,7 @@
 
 if [[ -f "/usr/bin/swayidle" ]]; then
 	echo "SwayIdle is installed"
-	swayidle -w timeout 120 'swaylock' timeout 300 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock'
+	swayidle -w timeout 300 'swaylock' timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock'
 else
 	echo "SwayIdle not Installed"
 fi
