4c4c04c3df85be71923146c3cfc7f2c197ef2881
Robin <robinludan@gmx.com>
2024-04-16 08:46:54 +0300

    cleanup staff I never use

--- .config/scripts/swayidle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [[ -f "/usr/bin/swayidle" ]]; then
-	echo "SwayIdle is installed"
-	swayidle -w timeout 300 'swaylock' timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock'
-else
-	echo "SwayIdle not Installed"
-fi
