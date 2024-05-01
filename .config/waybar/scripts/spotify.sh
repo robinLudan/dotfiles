fa18d1bd0412a0f94fd2b47ad111bddd6f3c8852
Robin <robinludan@gmx.com>
2024-05-02 01:27:52 +0300

    minimal

--- .config/waybar/scripts/spotify.sh
+++ .config/waybar/scripts/spotify.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# define the name of the player
-PLAYER="spotify"
+text="$(playerctl -p spotify metadata title) - $(playerctl -p spotify metadata artist)"
 
-# function to increase Spotify volume
-increase_volume() {
-	playerctl --player="$PLAYER" volume 0.05+
-}
-
-# function to decrease Spotify volume
-decrease_volume() {
-	playerctl --player="$PLAYER" volume 0.05-
-}
-
-# check for the argument and perform the corresponding action
-case "$1" in
-"u")
-	increase_volume
-	;;
-"d")
-	decrease_volume
-	;;
-*)
-	exit 1
-	;;
-esac
+echo "{\"text\": \"$text\"}"
