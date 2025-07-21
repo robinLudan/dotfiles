c8b6e04efa523f96a4177a181b162a656acb2b4a
Robin Ludan <robinludan@gmx.com>
2025-07-22 00:01:41 +0300

    add vaults to list

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find $HOME/dev/work $HOME/dev/personal $HOME/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find $HOME/dev/work $HOME/dev/personal $HOME/dotfiles $HOME/vaults -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
