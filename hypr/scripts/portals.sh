74056b0f2dd9208244a93a10c5219c6a5ea7453b
Robin Ludan <robinludan@gmx.com>
2025-08-20 21:50:53 +0300

    update portals

--- hypr/scripts/portals.sh
+++ hypr/scripts/portals.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-sleep 1
+#!/usr/bin/env bash
+sleep 4
 killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+sleep 4
 /usr/lib/xdg-desktop-portal &
