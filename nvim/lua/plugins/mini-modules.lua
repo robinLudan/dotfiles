79ec3a8e0bdc799fbb72aa963a5070c5b212f561
Robin Ludan <robinludan@gmx.com>
2025-10-24 20:05:25 +0300

    use autopairs from mini.nvim

--- nvim/lua/plugins/mini-modules.lua
+++ nvim/lua/plugins/mini-modules.lua
@@ -16,6 +16,7 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
+    require('mini.pairs').setup()
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
