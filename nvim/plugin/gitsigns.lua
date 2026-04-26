0cfa2a5f0ba68a46accfeeefc4390d7c647769c4
Robin Ludan <robinludan@gmx.com>
2026-04-26 23:29:19 +0300

    add gisigns config

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -10,4 +10,45 @@ require("gitsigns").setup({
 		topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
 		changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
 	},
+	current_line_blame = true,
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+	},
+	on_attach = function(bufnr)
+		local gitsigns = require("gitsigns")
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]g", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]g", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+			end
+		end)
+
+		map("n", "[g", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[g", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+			end
+		end)
+
+		map("n", "gp", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "gp", bang = true })
+			else
+				gitsigns.preview_hunk()
+			end
+		end)
+	end,
 })
