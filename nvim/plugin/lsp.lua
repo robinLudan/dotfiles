932e49594b389e0901425f3f9e8aa3f0762a77ef
Robin Ludan <robinludan@gmx.com>
2026-04-26 20:56:43 +0300

    add version tag to mason

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim", version = vim.version.range("v2.*") },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 	{ src = "https://github.com/j-hui/fidget.nvim" },
