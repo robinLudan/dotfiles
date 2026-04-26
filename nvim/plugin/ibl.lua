3f6bad9bebe7d3ab464068db371e1eed0a4d5b79
Robin Ludan <robinludan@gmx.com>
2026-04-26 23:38:38 +0300

    add indent blank line

---
+++ nvim/plugin/ibl.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		vim.pack.add({
+			{ src = "https://github.com/lukas-reineke/indent-blankline.nvim", name = "ibl" },
+		})
+		require("ibl").setup()
+	end,
+})
