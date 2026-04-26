7c47e4562e9db71b0c9c2bf1f2ef79820eb413f8
Robin Ludan <robinludan@gmx.com>
2026-04-26 22:58:55 +0300

    add formatters

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -10,12 +10,31 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		sh = { "shfmt" },
+		php = function(bufnr)
+			if require("conform").get_formatter_info("pint", bufnr).available then
+				return { "pint" }
+			else
+				return { "pretty-php" }
+			end
+		end,
+		vue = { "prettierd" },
+		html = { "prettierd" },
+		css = { "prettierd" },
+		scss = { "prettierd" },
+		graphql = { "prettierd" },
+		json = { "prettierd" },
+		jsonc = { "prettierd" },
+		yaml = { "prettierd" },
 		javascript = { "prettierd" },
-		-- other examples on usage
-		-- javascript = { 'prettierd', 'prettier', stop_after_first = true },
-		-- Conform will run multiple formatters sequentially
-		-- python = { "isort", "black" },
-		-- You can customize some of the format options for the filetype (:help conform.format)
-		-- rust = { "rustfmt", lsp_format = "fallback" },
+		typescript = { "prettierd" },
+		markdown = { "prettierd" },
+		go = { "goimports", "gofumpt" },
+		javascriptreact = { "prettierd" },
+		typescriptreact = { "prettierd" },
+	},
+	formatters = {
+		pint = {
+			command = "vendor/bin/pint",
+		},
 	},
 })
