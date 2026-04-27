90d5a14dce6fd649d67bbcdb7d85660a721de9b3
Robin Ludan <robinludan@gmx.com>
2026-04-27 10:35:16 +0300

    remove CTRL+<hjkl> to use tmux navigator instead

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -29,14 +29,6 @@ vim.diagnostic.config {
 map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- playback macros
 map('n', 'Q', '@qj', opts)
 map('x', 'Q', ':norm @q<CR>', opts)
